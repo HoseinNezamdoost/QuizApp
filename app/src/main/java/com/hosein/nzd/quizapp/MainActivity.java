49abeb01ed750c51d14f4491b15585b75654e73f
hosein.nzd <hosein.nzd26@gmail.com>
2022-04-24 01:14:43 +0430

    add start page into main activity

--- app/src/main/java/com/hosein/nzd/quizapp/MainActivity.java
+++ app/src/main/java/com/hosein/nzd/quizapp/MainActivity.java
@@ -2,13 +2,68 @@ package com.hosein.nzd.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialButton buttonStartGame , buttonExitApp;
+    RelativeLayout layout_start_parent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupStartPage();
+
     }
+
+    private void setupStartPage(){
+
+        buttonStartGame = findViewById(R.id.button_start_game);
+        layout_start_parent = findViewById(R.id.layout_start_parent);
+        buttonExitApp = findViewById(R.id.button_exit_app);
+
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0,2);
+        alphaAnimation.setDuration(500);
+        alphaAnimation.setFillAfter(true);
+        alphaAnimation.setRepeatCount(Animation.INFINITE);
+        alphaAnimation.setRepeatMode(Animation.REVERSE);
+        buttonStartGame.startAnimation(alphaAnimation);
+
+        ScaleAnimation scaleAnimation = new ScaleAnimation(1,1.5f , 1,1.5f ,
+                Animation.RELATIVE_TO_SELF , 0.5f , Animation.RELATIVE_TO_SELF , 0.5f);
+        scaleAnimation.setDuration(500);
+        scaleAnimation.setFillAfter(true);
+        scaleAnimation.setRepeatCount(Animation.INFINITE);
+        scaleAnimation.setRepeatMode(Animation.REVERSE);
+
+
+        AnimationSet set = new AnimationSet(true);
+        set.addAnimation(scaleAnimation);
+        set.addAnimation(alphaAnimation);
+        buttonStartGame.startAnimation(set);
+
+        buttonStartGame.setOnClickListener(view -> {
+            layout_start_parent.setVisibility(View.GONE);
+        });
+
+        buttonExitApp.setOnClickListener(view -> {
+            finish();
+        });
+
+    }
+
 }
